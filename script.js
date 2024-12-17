ec7d812172078809f57feddf56475f6e5133d31b
Samarth233-beeo <singh.samarthsam.7026@gmail.com>
2024-12-17 22:30:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
-const API_KEY = "AIzaSyBdEk8CG0pwV0RqryyRLX-FEBtP5PeMCjM";
+const API_KEY = "YOUR API KEY HERE";
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 // Initialize user message and file data
